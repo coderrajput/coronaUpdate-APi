4dad6456c2f5d5b5f6d33b11471bf1595a266bca
prateek singh <56526264+coderrajput@users.noreply.github.com>
2020-05-23 16:49:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -46,10 +46,10 @@ app.get('*', function(req, res){
 });
 
 app.use("/incoming",(req,res,next)=>{
-  let 
+  
   axios.post('https://api.telegram.org/bot1239110605:AAGshkiCwoIIlTxj7L0HlkfZJ4M132gkGGI/sendMessage',{
                     chat_id: "850830218",
-                    text: `${req}`
+                    text: "jai sri radhe"
                 }).then(resp=>{
                     console.log(resp.data.result);
                 }).catch(err=>console.log(err));
